161ed49cdc96213fae1a3d894a4c16127aa42b52
Nick Wells <wells.nicholas@gmail.com>
2020-04-16 16:23:07 +0100

    Simplify the error type names to reduce stutter

--- fsm/fsmError.go
+++ fsm/fsmError.go
@@ -2,38 +2,49 @@ package fsm
 
 import "fmt"
 
-// FSMErrUnknownState is an error type that represents an unknown state
-type FSMErrUnknownState struct {
+// Error is the type of an error from this package
+type Error interface {
+	error
+
+	// FSMError is a no-op function but it serves to
+	// distinguish errors from this package from other errors
+	FSMError()
+}
+
+// UnknownState is an error type that represents an unknown state
+type UnknownState struct {
 	FSMName string
 	State   string
 }
 
-// mkErrUnknownState constructs and returns an unknown state error
-func (f FSM) mkErrUnknownState(s string) FSMErrUnknownState {
-	return FSMErrUnknownState{
+// mkErrUnknownState constructs and returns an UnknownState error
+func (f FSM) mkErrUnknownState(s string) UnknownState {
+	return UnknownState{
 		FSMName: f.Name(),
 		State:   s,
 	}
 }
 
 // Error returns a string form of the error
-func (fe FSMErrUnknownState) Error() string {
+func (fe UnknownState) Error() string {
 	return fmt.Sprintf("FSM: %q: %q is not a known state",
 		fe.FSMName, fe.State)
 }
 
-// FSMErrNoTransition is an error type that represents a non-existent
+func (UnknownState) FSMError() {}
+
+// NoTransition is an error type that represents a non-existent
 // transition between states. This is the case where the fsm has no single
 // step between the two states.
-type FSMErrNoTransition struct {
+type NoTransition struct {
 	FSMName   string
 	FromState string
 	ToState   string
 }
 
-// mkErrNoTransition constructs and returns an unknown state error
-func (f FSM) mkErrNoTransition(s string) FSMErrNoTransition {
-	return FSMErrNoTransition{
+// mkErrNoTransition constructs and returns a NoTransition error
+func (f FSM) mkErrNoTransition(s string) NoTransition {
+	return NoTransition{
 		FSMName:   f.Name(),
 		FromState: f.current.name,
 		ToState:   s,
@@ -41,25 +52,27 @@ func (f FSM) mkErrNoTransition(s string) FSMErrNoTransition {
 }
 
 // Error returns a string form of the error
-func (fe FSMErrNoTransition) Error() string {
+func (fe NoTransition) Error() string {
 	return fmt.Sprintf("FSM: %q: There is no valid transition from %q to %q",
 		fe.FSMName, fe.FromState, fe.ToState)
 }
 
-// FSMErrForbiddenChange is an error type that represents a forbidden transition
+func (NoTransition) FSMError() {}
+
+// ForbiddenChange is an error type that represents a forbidden transition
 // between states. This is the case where the fsm would allow the transition
 // but the underlying check prevents it (TransitionAllowed returns a non-nil
 // error)
-type FSMErrForbiddenChange struct {
+type ForbiddenChange struct {
 	FSMName   string
 	FromState string
 	ToState   string
 	UndError  error
 }
 
-// mkErrForbiddenChange constructs and returns an unknown state error
-func (f FSM) mkErrForbiddenChange(s string, ue error) FSMErrForbiddenChange {
-	return FSMErrForbiddenChange{
+// mkErrForbiddenChange constructs and returns a ForbiddenChange error
+func (f FSM) mkErrForbiddenChange(s string, ue error) ForbiddenChange {
+	return ForbiddenChange{
 		FSMName:   f.Name(),
 		FromState: f.current.name,
 		ToState:   s,
@@ -67,8 +80,15 @@ func (f FSM) mkErrForbiddenChange(s string, ue error) FSMErrForbiddenChange {
 	}
 }
 
+// Unwrap returns the underlying error
+func (fe ForbiddenChange) Unwrap() error {
+	return fe.UndError
+}
+
 // Error returns a string form of the error
-func (fe FSMErrForbiddenChange) Error() string {
+func (fe ForbiddenChange) Error() string {
 	return fmt.Sprintf("FSM: %q: The change from %q to %q is forbidden: %s",
 		fe.FSMName, fe.FromState, fe.ToState, fe.UndError)
 }
+
+func (ForbiddenChange) FSMError() {}
